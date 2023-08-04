903d533243e301cdafbe4f22dda042c9b6151ea1
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-08-03 22:47:04 -0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const cors = require('cors')
 const productsRouter = require('./src/routes/products')
 const categoryRouter = require('./src/routes/categories')
 const settingsRouter = require('./src/routes/settings')
 const app = express()
 const port = 3333
 require('./db')
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://e-commerce-admin-flame.vercel.app"
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Access denied by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
