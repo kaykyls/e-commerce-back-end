fcbdcea4ae1d390296d1a84b4ad0840331ffb590
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-12-30 23:59:00 -0300

    update

--- src/routes/products.js
+++ src/routes/products.js
@@ -66,8 +66,6 @@ router.post("/add", checkToken, uploadMulter.array("files"), async (req, res) =>
   sizes = sizes.split(',').map(size => parseInt(size));
   colors = colors.split(',');
 
-  console.log(sizes)
-
   try {
     let product = new Product({
       title,
@@ -147,29 +145,81 @@ router.post("/add", checkToken, uploadMulter.array("files"), async (req, res) =>
   }
 });
 
-router.put("/:id/edit", async (req, res) => {
-  console.log(req.body)
+router.put("/:id/edit", uploadMulter.array("files"), async (req, res) => {
+  console.log(req.body);
 
   let { title, currentPrice, colors, sizes, description, categories, stock } = req.body;
-  
+
   try {
-    let product = await Product.findByIdAndUpdate(req.params.id, {
-      title,
-      currentPrice,
-      colors,
-      sizes,
-      description,
-      categories,
-      stock: JSON.parse(stock),
-      images: [],
-      prices: [],
-    }, { new: true });
+    let product = await Product.findById(req.params.id);
+
+    // Remova imagens antigas se necessário
+    // product.images = [];
+
+    const uploadPromises = req.files.map(async (file, index) => {
+      const name = Date.now() + "-" + file.originalname;
+      const storageRef = ref(storage, name);
+      const uploadTask = uploadBytesResumable(storageRef, file.buffer);
+
+      return new Promise((resolve, reject) => {
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          async () => {
+            try {
+              const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+              console.log("File available at", downloadURL);
+
+              product.images.push({
+                src: downloadURL,
+                color: JSON.parse(req.body.photosColors)[index].color
+              });
+
+              resolve();
+            } catch (err) {
+              console.log(err);
+              reject(err);
+            }
+          }
+        );
+      });
+    });
+
+    await Promise.all(uploadPromises);
+
+    // Atualize as outras propriedades
+    product.title = title;
+    product.currentPrice = currentPrice;
+    product.colors = colors.split(",");
+    product.sizes = sizes.split(",").map((size) => parseInt(size));
+    product.description = description;
+    product.categories = categories;
+    product.stock = JSON.parse(stock);
+
+    // Salve as alterações
+    await product.save();
 
     res.status(200).json(product);
   } catch (err) {
+    console.log(err);
     res.status(422).json(err);
   }
-})
+});
+
 
 router.delete("/delete/:id", async (req, res) => {
   try {
